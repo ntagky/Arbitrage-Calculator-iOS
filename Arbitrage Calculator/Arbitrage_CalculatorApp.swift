6d84ab2fcc492e778c7e98cf1ea6bee0306a4ecc
ntagky <ntagkonik@csd.auth.gr>
2022-12-25 19:32:10 +0200

    Initial Commit

---
+++ Arbitrage Calculator/Arbitrage_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arbitrage_CalculatorApp.swift
+//  Arbitrage Calculator
+//
+//  Created by Alexandros Ntagkonikos on 25/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Arbitrage_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
